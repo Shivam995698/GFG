cc17e7fac04857ba1bdf8fe7b3fa93d222fcd694
Shivam Verma <128091749+Shivam995698@users.noreply.github.com>
2025-09-22 18:17:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Anagram/anagram.java
+++ Difficulty: Easy/Anagram/anagram.java
@@ -1,20 +1,18 @@
 class Solution {
     public static boolean areAnagrams(String s1, String s2) {
-        // code here
         int n1 = s1.length();
         int n2 = s2.length();
-        HashMap<Character,Integer>mp1 = new HashMap<>();
+        HashMap<Character,Integer> mp1 = new HashMap<>();
         HashMap<Character,Integer>mp2 = new HashMap<>();
-        for(int i=0;i<n1;i++)
+        for(char ch:s1.toCharArray())
         {
-            char ch = s1.charAt(i);
             mp1.put(ch,mp1.getOrDefault(ch,0)+1);
         }
-        for(int i=0;i<n2;i++)
+       for(char ch:s2.toCharArray())
         {
-            char ch = s2.charAt(i);
             mp2.put(ch,mp2.getOrDefault(ch,0)+1);
         }
         return mp1.equals(mp2);
+        
     }
 }
