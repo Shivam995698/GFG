2ea44ce3c81692362f932370563ea12d30b290df
Shivam Verma <128091749+Shivam995698@users.noreply.github.com>
2025-09-22 17:00:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Length of String/length-of-string.cpp
@@ -0,0 +1,10 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int lengthString(string &s) {
+        // Your code here
+        int n = s.length();
+        return n;
+    }
+};
