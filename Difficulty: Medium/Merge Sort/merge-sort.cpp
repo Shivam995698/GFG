c9e5c6ea376fa5418682ee7e471f936c8db4bd30
Shivam Verma <128091749+Shivam995698@users.noreply.github.com>
2026-01-08 20:50:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.cpp
+++ Difficulty: Medium/Merge Sort/merge-sort.cpp
@@ -1,42 +1,40 @@
 class Solution {
-  public:   
-  void merge(int l,int mid,int r,vector<int>&arr)
+  public:
+    void merge(int l,int mid,int r,vector<int>&ans)
     {
-        // int n = arr.size();
         vector<int>temp;
         int i = l;
         int j = mid+1;
         while(i<=mid && j<=r)
         {
-            if(arr[i]<=arr[j])
+            if(ans[i]<ans[j])
             {
-                temp.push_back(arr[i]);
+                temp.push_back(ans[i]);
                 i++;
             }
             else{
-                temp.push_back(arr[j]);
+                temp.push_back(ans[j]);
                 j++;
             }
         }
         while(i<=mid)
         {
-            temp.push_back(arr[i]);
+            temp.push_back(ans[i]);
             i++;
         }
         while(j<=r)
         {
-            temp.push_back(arr[j]);
+            temp.push_back(ans[j]);
             j++;
         }
-        //coping in original arr 
-        int n = temp.size();
+        int n =temp.size();
         for(int i = 0;i<n;i++)
         {
-            arr[l+i]= temp[i];
+            ans[l+i]=temp[i];
         }
     }
     void mergeSort(vector<int>& arr, int l, int r) {
-        // code here
+        int n = arr.size();
         if(l>=r)
         {
             return;
@@ -45,6 +43,5 @@ class Solution {
         mergeSort(arr,l,mid);
         mergeSort(arr,mid+1,r);
         merge(l,mid,r,arr);
-        
     }
 };
